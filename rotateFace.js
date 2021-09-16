77b0d6e768613e029eb58e1d6717054c794aaa35
Myron Giannakis <86615442+G-Myron@users.noreply.github.com>
2021-09-17 01:56:34 +0300

    Add files via upload

--- rotateFace.js
+++ rotateFace.js
@@ -1,10 +1,20 @@
 let faces = document.querySelectorAll(".face");
 
+rotaionsList = [
+    "rotateY(0deg) translateZ(35vmin)",
+    "rotateY(180deg) translateZ(35vmin)",
+    "rotateY(90deg) translateZ(35vmin)",
+    "rotateY(-90deg) translateZ(35vmin)",
+    "rotateX(90deg) translateZ(35vmin)",
+    "rotateX(-90deg) translateZ(35vmin)"
+]
 
-faces.forEach( (face)=> {
+faces.forEach( (face, iter)=> {
     let x0,y0, moving=false;
     let [initialAngX, initialAngY] = [0, 0];
     let currentAngX, currentAngY;
+
+    face.style.transform = rotaionsList[iter];
 
     face.addEventListener("mousedown", (event) => faceTouch(event.x,event.y));
     document.addEventListener("mousemove", (event) => rotateFace(event.x,event.y));
@@ -15,18 +25,19 @@ faces.forEach( (face)=> {
         [x0,y0] = [x,y];
     }
 
-    function rotateFace(x, y, friction = 4){
+    function rotateFace(x, y, friction = 2){
         if(!moving) return;
     
         dx = x - x0;
         dy = y - y0;
+        [x0,y0] = [x,y];
     
         angY =  dx / friction;
         angX = -dy / friction;
     
         [currentAngX, currentAngY] = [initialAngX + angX, initialAngY + angY];
     
-        face.style.transform = `rotateY(${currentAngY}deg) translateZ(35vmin)`;
+        face.style.transform += `rotate(${currentAngY}deg)`;
     }
 
     function endFace(){
